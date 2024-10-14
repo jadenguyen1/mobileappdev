4df5d4c2c7e4f8e7315d0d1589d6593bf6875ae2
Jade Nguyen <jadenguyen@academic.rrc.ca>
2024-10-14 14:51:36 -0500

    test

---
+++ app/src/main/java/com/example/test/Demo.kt
@@ -0,0 +1,23 @@
+package com.example.test
+
+fun main () {
+    // Test
+
+    /*
+    Test
+     */
+
+    println("Hello Kotlin")
+
+    val num = 10 // read-only
+
+    var num2 = 33 // mutable
+
+    val multiLineString = """
+        Line 1
+        Line 2
+        Line 3
+    """
+
+    print(multiLineString)
+}
